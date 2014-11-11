11e37cd9c1ba3fde011759b03065a693c9b2f458
Dylan McDermott <dm606@cam.ac.uk>
2014-11-11 18:54:20 +0000

    Added more unit tests

--- tests/test_syntax.ml
+++ tests/test_syntax.ml
@@ -11,24 +11,25 @@ let test_eq_parser (name, input, expected) =
 
 let test_parser = "parser" >::: (map test_eq_parser [
   ("t1", "U;;", ReplExpression (EUniverse, SEMISEMI ";;"));
-  ("t2", "let x : U = U;;", ReplDeclaration ([DLet (Ident "x", EUniverse,
+  ("t2", "let x : U = U;;", ReplDeclaration ([DLet (Ident "x", [], EUniverse,
   EUniverse)], SEMISEMI ";;"));
   ("t3", "let f : Nat -> Nat * Bool -> Nat = fun x _ -> succ x;;",
-  ReplDeclaration ([DLet (Ident "f", EArrow (EIdentifier (Ident "Nat"), EArrow
-  (ETimes (EIdentifier (Ident "Nat"), EIdentifier (Ident "Bool")), EIdentifier
-  (Ident "Nat"))), ELambda ([BName (Ident "x"); BUnderscore], EApplication
-  (EIdentifier (Ident "succ"), EIdentifier (Ident "x"))))], SEMISEMI ";;"));
+  ReplDeclaration ([DLet (Ident "f", [], EArrow (EIdentifier (Ident "Nat"),
+  EArrow (ETimes (EIdentifier (Ident "Nat"), EIdentifier (Ident "Bool")),
+  EIdentifier (Ident "Nat"))), ELambda ([BName (Ident "x"); BUnderscore],
+  EApplication (EIdentifier (Ident "succ"), EIdentifier (Ident "x"))))],
+  SEMISEMI ";;"));
   ("t4", "fun A -> A -> Empty;;", ReplExpression (ELambda ([BName (Ident "A")],
   EArrow (EIdentifier (Ident "A"), EIdentifier (Ident "Empty"))), SEMISEMI
   ";;"));
   ("t5", "function;;", ReplExpression (EFunction [], SEMISEMI ";;"));
   ("t6", "let rec f : Nat = zero and type Nat = zero : Nat | succ : (_:Nat) ->
-  Nat;;", ReplDeclaration ([DLetRec (Ident "f", EIdentifier (Ident "Nat"),
+  Nat;;", ReplDeclaration ([DLetRec (Ident "f", [], EIdentifier (Ident "Nat"),
   EIdentifier (Ident "zero")); DSimpleType (Ident "Nat", [Constr (Ident
   "zero", EIdentifier (Ident "Nat")); Constr (Ident "succ", EPi (BUnderscore,
   EIdentifier (Ident "Nat"), EIdentifier (Ident "Nat")))])], SEMISEMI ";;"));
   ("t7", "let f : U = U in type Empty = in Empty;;", ReplExpression
-  (EDeclaration ([DLet (Ident "f", EUniverse, EUniverse)], EDeclaration
+  (EDeclaration ([DLet (Ident "f", [], EUniverse, EUniverse)], EDeclaration
   ([DSimpleType (Ident "Empty", [])], EIdentifier (Ident "Empty"))), SEMISEMI
   ";;"));
   ("t8", "type Id (A : U) (a : A) : A -> U = | refl : Id A a a;;",
@@ -42,7 +43,11 @@ let test_parser = "parser" >::: (map test_eq_parser [
   EIdentifier (Ident "d")), SEMISEMI ";;"));
   ("t10", "(x, y, z).1;;", ReplExpression (EProj1 (EPair (EIdentifier (Ident
   "x"), EPair (EIdentifier (Ident "y"), EIdentifier (Ident "z")))), SEMISEMI
-  ";;"))
+  ";;"));
+  ("t9", "let rec f (x : A) (_ : B) : U = fun z -> U;;", ReplDeclaration
+  ([DLetRec (Ident "f", [Param (BName (Ident "x"), EIdentifier (Ident "A"));
+  Param (BUnderscore, EIdentifier (Ident "B"))], EUniverse, ELambda ([BName
+  (Ident "z")], EUniverse))], SEMISEMI ";;"))
   ])
 
 let test_eq_desugar (name, input, expected) =
@@ -56,21 +61,28 @@ let test_desugar = "desugar" >::: (map test_eq_desugar [
   "y"), Universe)], Identifier "x"))));
   ("t2", ReplExpression (EDeclaration ([DSimpleType (Ident "Bool", [Constr
   (Ident "false", EIdentifier (Ident "Bool")); Constr (Ident "true", EIdentifier
-  (Ident "Bool"))]); DLetRec (Ident "x", EUnitType, EUnit) ], EUnit), SEMISEMI
-  ";;"), Expression (LocalDeclaration ([Type ("Bool", [], Universe, [("false",
-  Identifier "Bool"); ("true", Identifier "Bool")]); LetRec ("x", UnitType,
-  Unit) ], Unit)));
+  (Ident "Bool"))]); DLetRec (Ident "x", [], EUnitType, EUnit) ], EUnit),
+  SEMISEMI ";;"), Expression (LocalDeclaration ([Type ("Bool", [], Universe,
+  [("false", Identifier "Bool"); ("true", Identifier "Bool")]); LetRec ("x",
+  UnitType, Unit) ], Unit)));
   ("t3", ReplExpression (ETimes (EUniverse, EUniverse), SEMISEMI ";;"),
   Expression (Sigma (Underscore, Universe, Universe)));
   ("t4", ReplDeclaration ([DType (Ident "Vec", [Param (BName (Ident "A"),
-  EUniverse)], EArrow (EIdentifier (Ident "Nat"), EUniverse), [ Constr (Ident
+  EUniverse)], EArrow (EIdentifier (Ident "Nat"), EUniverse), [Constr (Ident
   "nil", EApplication (EApplication (EIdentifier (Ident "Vec"), EIdentifier
   (Ident "A")), EIdentifier (Ident "zero"))) ])], SEMISEMI ";;"), Declaration
   [Type ("Vec", [(Name "A", Universe)], Pi (Underscore, Identifier "Nat",
   Universe), [ ("nil", Application (Application (Identifier "Vec", Identifier
   "A"), Identifier "zero")) ])]);
-  ("t5", ReplExpression (ELambda ([BName (Ident "x"); BName (Ident "y")], EUnit),
-  SEMISEMI ";;"), Expression (Lambda (Name "x", Lambda (Name "y", Unit))))
+  ("t5", ReplExpression (ELambda ([BName (Ident "x"); BName (Ident "y")],
+  EUnit), SEMISEMI ";;"), Expression (Lambda (Name "x", Lambda (Name "y",
+  Unit))));
+  ("t6", ReplDeclaration ([DLet (Ident "f", [Param (BName (Ident "x"),
+  EUniverse); Param (BName (Ident "y"), EUnitType); Param (BUnderscore,
+  EIdentifier (Ident "A"))], EUniverse, EUnitType)], SEMISEMI ";;"), Declaration
+  ([Let ("f", Pi (Name "x", Universe, Pi (Name "y", UnitType, Pi (Underscore,
+  Identifier "A", Universe))), Lambda (Name "x", Lambda (Name "y", Lambda
+  (Underscore, UnitType))))]))
   ])
 
 let test_eq_resugar (name, input, expected) =
@@ -82,23 +94,31 @@ let test_resugar = "resugar" >::: (map test_eq_resugar [
   Identifier "x"))), ReplExpression (EArrow (EIdentifier (Ident "Nat"), EMatch
   (EIdentifier (Ident "x"), [CCase (PPair (PUnderscore, PIdentifier (Ident
   "y")), EUniverse)])), SEMISEMI ";;"));
-  ("t2", Expression (LocalDeclaration ([ Type ("Bool", [], Universe, [("false",
+  ("t2", Expression (LocalDeclaration ([Type ("Bool", [], Universe, [("false",
   Identifier "Bool"); ("true", Identifier "Bool")]); LetRec ("x", UnitType,
-  Unit) ], Unit)), ReplExpression (EDeclaration ([ DSimpleType (Ident "Bool",
+  Unit) ], Unit)), ReplExpression (EDeclaration ([DSimpleType (Ident "Bool",
   [Constr (Ident "false", EIdentifier (Ident "Bool")); Constr (Ident "true",
-  EIdentifier (Ident "Bool"))]); DLetRec (Ident "x", EUnitType, EUnit) ],
+  EIdentifier (Ident "Bool"))]); DLetRec (Ident "x", [], EUnitType, EUnit) ],
   EUnit), SEMISEMI ";;"));
   ("t3", Expression (Sigma (Underscore, Universe, Universe)), ReplExpression
   (ETimes (EUniverse, EUniverse), SEMISEMI ";;"));
   ("t4", Declaration [Type ("Vec", [(Name "A", Universe)], Pi (Underscore,
-  Identifier "Nat", Universe), [ ("nil", Application (Application (Identifier
+  Identifier "Nat", Universe), [("nil", Application (Application (Identifier
   "Vec", Identifier "A"), Identifier "zero")) ])], ReplDeclaration ([DType
   (Ident "Vec", [Param (BName (Ident "A"), EUniverse)], EArrow (EIdentifier
-  (Ident "Nat"), EUniverse), [ Constr (Ident "nil", EApplication (EApplication
+  (Ident "Nat"), EUniverse), [Constr (Ident "nil", EApplication (EApplication
   (EIdentifier (Ident "Vec"), EIdentifier (Ident "A")), EIdentifier (Ident
   "zero"))) ])], SEMISEMI ";;"));
   ("t5", Expression (Lambda (Name "x", Lambda (Name "y", Unit))), ReplExpression
-  (ELambda ([BName (Ident "x"); BName (Ident "y")], EUnit), SEMISEMI ";;"))
+  (ELambda ([BName (Ident "x"); BName (Ident "y")], EUnit), SEMISEMI ";;"));
+  ("t6", Declaration ([Let ("f", Pi (Underscore, Identifier "A", Pi (Name "x",
+  Identifier "B", Pi (Name "y", Identifier "C", Pi (Underscore, Identifier "D",
+  UnitType)))), Lambda (Underscore, Lambda (Name "x", Lambda (Name "y", Lambda
+  (Name "z", Unit)))))]), ReplDeclaration ([DLet ( Ident "f", [Param
+  (BUnderscore, EIdentifier (Ident "A")); Param (BName (Ident "x"), EIdentifier
+  (Ident "B")); Param (BName (Ident "y"), EIdentifier (Ident "C"))], EArrow
+  (EIdentifier (Ident "D"), EUnitType), ELambda ([BName (Ident "z")], EUnit))],
+  SEMISEMI ";;"))
   ])
 
 let test_syntax = "syntax" >::: [test_parser; test_desugar; test_resugar]
