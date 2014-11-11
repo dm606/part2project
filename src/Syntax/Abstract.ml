4590ea4ad5a8559e2526b75e5aaa4d8a58692bd1
Dylan McDermott <dm606@cam.ac.uk>
2014-11-11 19:14:10 +0000

    Renaming

--- src/Syntax/Abstract.ml
+++ src/Syntax/Abstract.ml
@@ -9,9 +9,9 @@ type expression_or_declaration =
   | Declaration of declaration list
 and expression =
   | Pair of expression * expression
-  | Lambda of binding * expression
-  | Pi of binding * expression * expression
-  | Sigma of binding * expression * expression
+  | Lambda of binder * expression
+  | Pi of binder * expression * expression
+  | Sigma of binder * expression * expression
   | Function of (pattern * expression) list
   | LocalDeclaration of declaration list * expression
   | Application of expression * expression
@@ -21,7 +21,7 @@ and expression =
   | Identifier of string
   | Proj1 of expression
   | Proj2 of expression
-and binding =
+and binder =
   | Underscore
   | Name of string 
 and pattern =
@@ -32,7 +32,7 @@ and pattern =
 and declaration =
   | Let of string * expression * expression
   | LetRec of string * expression * expression
-  | Type of string * (binding * expression) list
+  | Type of string * (binder * expression) list
           * expression * (string * expression) list
 
 let rec desugar = function
@@ -42,14 +42,14 @@ and desugar_expression = function
   | EPair (e1, e2) -> Pair (desugar_expression e1, desugar_expression e2)
   | ELambda ([], _) ->
       raise (Invalid_expression "Lambda abstractions must have at least one 
-      binding")
-  | ELambda ([x], e1) -> Lambda (desugar_binding x, desugar_expression e1)
+      binder")
+  | ELambda ([x], e1) -> Lambda (desugar_binder x, desugar_expression e1)
   | ELambda (x::xs, e1) ->
-      Lambda (desugar_binding x, desugar_expression (ELambda (xs, e1)))
+      Lambda (desugar_binder x, desugar_expression (ELambda (xs, e1)))
   | EPi (x, e1, e2) ->
-      Pi (desugar_binding x, desugar_expression e1, desugar_expression e2)
+      Pi (desugar_binder x, desugar_expression e1, desugar_expression e2)
   | ESigma (x, e1, e2) -> 
-      Sigma (desugar_binding x, desugar_expression e1, desugar_expression e2)
+      Sigma (desugar_binder x, desugar_expression e1, desugar_expression e2)
   | EFunction xs -> Function (map desugar_case xs) 
   | EDeclaration (d, e1) ->
       LocalDeclaration (map desugar_declaration d, desugar_expression e1)
@@ -72,8 +72,8 @@ and desugar_declaration = function
       let rec handle_parameters t e = function
         | [] -> (t, e)
         | (Param (b, e1))::ps ->
-            handle_parameters (Pi (desugar_binding b, desugar_expression e1, t))
-            (Lambda (desugar_binding b, e)) ps in
+            handle_parameters (Pi (desugar_binder b, desugar_expression e1, t))
+            (Lambda (desugar_binder b, e)) ps in
       let t, e =
         handle_parameters (desugar_expression e1) (desugar_expression e2) (rev ps) in
       Let (x, t, e)
@@ -81,8 +81,8 @@ and desugar_declaration = function
       let rec handle_parameters t e = function
         | [] -> (t, e)
         | (Param (b, e1))::ps ->
-            handle_parameters (Pi (desugar_binding b, desugar_expression e1, t))
-            (Lambda (desugar_binding b, e)) ps in
+            handle_parameters (Pi (desugar_binder b, desugar_expression e1, t))
+            (Lambda (desugar_binder b, e)) ps in
       let t, e =
         handle_parameters (desugar_expression e1) (desugar_expression e2) (rev ps) in
       LetRec (x, t, e)
@@ -90,13 +90,13 @@ and desugar_declaration = function
       , desugar_expression e1, map desugar_constructor cs)
   | DSimpleType (Ident x, cs) -> 
       Type (x, [], Universe, map desugar_constructor cs)
-and desugar_binding = function 
+and desugar_binder = function 
   | BName (Ident id) -> Name id
   | BUnderscore -> Underscore
 and desugar_case = function
   | CCase (p, e) -> (desugar_pattern p, desugar_expression e)
 and desugar_parameter = function
-  | Param (x, e) -> (desugar_binding x, desugar_expression e)
+  | Param (x, e) -> (desugar_binder x, desugar_expression e)
 and desugar_constructor = function
   | Constr (Ident x, e) -> (x, desugar_expression e)
 and desugar_pattern = function
@@ -112,19 +112,19 @@ let rec resugar = function
 and resugar_expression = function
   | Pair (e1, e2) -> EPair (resugar_expression e1, resugar_expression e2)
   | Lambda (b, e) ->
-      let rec collect_bindings l = function
-        | Lambda (b, e) -> collect_bindings (b::l) e
+      let rec collect_binders l = function
+        | Lambda (b, e) -> collect_binders (b::l) e
         | e -> (l, e) in
-      let bs, e = collect_bindings [b] e in
-      ELambda (rev (map resugar_binding bs), resugar_expression e)
+      let bs, e = collect_binders [b] e in
+      ELambda (rev (map resugar_binder bs), resugar_expression e)
   | Pi (Underscore, e1, e2) -> 
       EArrow (resugar_expression e1, resugar_expression e2)
   | Pi (b, e1, e2) ->
-      EPi (resugar_binding b, resugar_expression e1, resugar_expression e2)
+      EPi (resugar_binder b, resugar_expression e1, resugar_expression e2)
   | Sigma (Underscore, e1, e2) ->
       ETimes (resugar_expression e1, resugar_expression e2)
   | Sigma (b, e1, e2) -> 
-      ESigma (resugar_binding b, resugar_expression e1, resugar_expression e2)
+      ESigma (resugar_binder b, resugar_expression e1, resugar_expression e2)
   | Function cs -> EFunction
       (map (fun (p, e) -> CCase (resugar_pattern p, resugar_expression e)) cs)
   | LocalDeclaration (ds, e) ->
@@ -143,17 +143,17 @@ and resugar_declaration = function
   | Let (x, e1, e) ->
       (match resugar_expression e with
        | ELambda (bs, e) ->
-           let rec collect_bindings ps t = function
+           let rec collect_binders ps t = function
              | [] -> (ps, t, [])
              | b::bs ->
                  (match t with
                   | Pi (b2, e1, e2) ->
-                      let b2, e1 = resugar_binding b2, resugar_expression e1 in
+                      let b2, e1 = resugar_binder b2, resugar_expression e1 in
                       if b = b2 then
-                        collect_bindings ((Param (b, e1))::ps) e2 bs
+                        collect_binders ((Param (b, e1))::ps) e2 bs
                       else (ps, t, b::bs)
                   | _ -> (ps, t, b::bs)) in
-           (match collect_bindings [] e1 bs with 
+           (match collect_binders [] e1 bs with 
             | ps, t, [] -> DLet (Ident x, rev ps, resugar_expression t, e)
             | ps, t, bs ->
                 DLet (Ident x, rev ps, resugar_expression t, ELambda (bs, e)))
@@ -161,17 +161,17 @@ and resugar_declaration = function
   | LetRec (x, e1, e) ->
       (match resugar_expression e with
        | ELambda (bs, e) ->
-           let rec collect_bindings ps t = function
+           let rec collect_binders ps t = function
              | [] -> (ps, t, [])
              | b::bs ->
                  (match t with
                   | Pi (b2, e1, e2) ->
-                      let b2, e1 = resugar_binding b2, resugar_expression e1 in
+                      let b2, e1 = resugar_binder b2, resugar_expression e1 in
                       if b = b2 then
-                        collect_bindings ((Param (b, e1))::ps) e2 bs
+                        collect_binders ((Param (b, e1))::ps) e2 bs
                       else (ps, t, b::bs)
                   | _ -> (ps, t, b::bs)) in
-           (match collect_bindings [] e1 bs with 
+           (match collect_binders [] e1 bs with 
             | ps, t, [] -> DLetRec (Ident x, rev ps, resugar_expression t, e)
             | ps, t, bs -> DLetRec
                 (Ident x, rev ps, resugar_expression t, ELambda (bs, e)))
@@ -181,10 +181,10 @@ and resugar_declaration = function
       , map (fun (x, e) -> Constr (Ident x, resugar_expression e)) cs)
   | Type (x, ps, e, cs) ->
       DType (Ident x
-      , map (fun (b, e) -> Param (resugar_binding b, resugar_expression e)) ps
+      , map (fun (b, e) -> Param (resugar_binder b, resugar_expression e)) ps
       , resugar_expression e
       , map (fun (x, e) -> Constr (Ident x, resugar_expression e)) cs)
-and resugar_binding = function
+and resugar_binder = function
   | Underscore -> BUnderscore
   | Name x -> BName (Ident x)
 and resugar_pattern = function
