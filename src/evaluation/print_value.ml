8fa3a1b25bad5f5f4508e0bb18150af027cdfae8
Dylan McDermott <dm606@cam.ac.uk>
2014-12-07 15:14:23 +0000

    Changed value printer

---
+++ src/evaluation/print_value.ml
@@ -0,0 +1,34 @@
+open Format
+
+open Value
+
+let rec pr_value fmt = function
+  | VPair (v1, v2) -> fprintf fmt "@[<hov2>%a,@ %a@]" pr_value3 v1 pr_value v2
+  | v -> pr_value1 fmt v
+and pr_value1 fmt = function
+  | VLambda (_, _, _) -> fprintf fmt "<fun>"
+  | VFunction (_, _) -> fprintf fmt "<function>"
+  | v -> pr_value2 fmt v
+and pr_value2 fmt = function
+  | VPi (_, _, _, _) -> fprintf fmt "<pi>"
+  | v -> pr_value3 fmt v
+and pr_value3 fmt = function
+  | VSigma (_, _, _, _) -> fprintf fmt "<sigma>"
+  | v -> pr_value4 fmt v
+and pr_value4 fmt = function
+  | VConstruct (c, (_::_ as l)) ->
+      let rec pr_values fmt = function
+        | [] -> assert false
+        | [v] -> pr_value5 fmt v
+        | v::tl ->
+            fprintf fmt "@[<hov2>%a@ %a@]" pr_value5 v pr_values tl in
+      fprintf fmt "@[%s@ %a@]" c pr_values (List.rev l)
+  | v -> pr_value5 fmt v
+and pr_value5 fmt = function
+  | VConstruct (c, []) -> fprintf fmt "%s" c
+  | VUniverse -> fprintf fmt "U"
+  | VUnitType -> fprintf fmt "Unit"
+  | VUnit -> fprintf fmt "()"
+  | v -> fprintf fmt "(%a)" pr_value v
+
+let print_value v = pr_value std_formatter v; print_newline ()
--- src/main.ml
+++ src/main.ml
@@ -9,6 +9,7 @@ open Abstract
 open BNFC_Util
 open Eval
 open Parser
+open Print_value
 open Value
 
 exception Unknown_command of string
@@ -35,112 +36,6 @@ let reset_position lexbuf filename =
     pos_cnum = 0;
     pos_bol = 0
   }
-
-let name_counts = Hashtbl.create 256
-
-let get_binder_name x = 
-  try
-    let c = Hashtbl.find name_counts x + 1 in
-    Hashtbl.replace name_counts x c;
-    x ^ string_of_int c
-  with Not_found -> Hashtbl.add name_counts x 0; x
-
-let rec rename_binders = function
-  | PatternPair (p1, p2) -> PatternPair (rename_binders p1, rename_binders p2)
-  | PatternApplication (x, l) -> PatternApplication (x, map rename_binders l)
-  | PatternBinder x -> PatternBinder (get_binder_name x)
-  | PatternUnderscore -> PatternUnderscore
-
-let rec perform_substitutions nesting_level env = function
-  | Pair (p1, p2) ->
-      Pair (perform_substitutions nesting_level env p1
-          , perform_substitutions nesting_level env p2)
-  | Lambda (Underscore, e) ->
-      Lambda (Underscore, perform_substitutions nesting_level env e)
-  | Lambda (Name x, e) ->
-      Lambda (Name (get_binder_name x)
-            , perform_substitutions (nesting_level + 1) env e)
-  | Pi (Underscore, e1, e2) ->
-      Pi (Underscore, perform_substitutions nesting_level env e1
-        , perform_substitutions nesting_level env e2)
-  | Pi (Name x, e1, e2) ->
-      Pi (Name (get_binder_name x), perform_substitutions nesting_level env e1
-        , perform_substitutions (nesting_level + 1) env e2)
-  | Sigma (Underscore, e1, e2) ->
-      Sigma (Underscore, perform_substitutions nesting_level env e1
-                      , perform_substitutions nesting_level env e2)
-  | Sigma (Name x, e1, e2) ->
-      Sigma (Name (get_binder_name x)
-           , perform_substitutions nesting_level env e1
-           , perform_substitutions (nesting_level + 1) env e2)
-  | Function l -> 
-      Function (map (fun (p, e) ->
-        (rename_binders p
-       , perform_substitutions (nesting_level + count_pattern_binders p)
-           env e)) l)
-  | LocalDeclaration (l, e) ->
-      let binder_count = count_declaration_binders l in
-      let subst_decl = function
-        | Let (x, e1, e2) ->
-            Let (get_binder_name x
-               , perform_substitutions (nesting_level + binder_count - 1) env e1
-               , perform_substitutions (nesting_level + binder_count - 1) env e2
-            )
-        | LetRec (x, e1, e2) ->
-            LetRec (get_binder_name x
-                  , perform_substitutions (nesting_level + binder_count - 1)
-                      env e1
-                  , perform_substitutions (nesting_level + binder_count) env e2) 
-        | Type (x, ps, e, cs) ->
-           let nl, ps = fold_left (fun (nl, l) (b, e) ->
-               match b with
-               | Underscore -> (nl, (b, perform_substitutions nl env e)::l)
-               | Name _ -> (nl + 1, (b, perform_substitutions nl env e)::l))
-            (nesting_level + binder_count, []) ps in
-           let cs =
-             map (fun (x, e) -> (x, perform_substitutions nl env e)) cs in
-           Type (get_binder_name x, ps, perform_substitutions nl env e, cs)
-      in
-      LocalDeclaration (map subst_decl l
-                      , perform_substitutions (nesting_level + binder_count)
-                          env e)
-  | Application (e1, e2) ->
-      Application (perform_substitutions nesting_level env e1
-                 , perform_substitutions nesting_level env e2)
-  | Index i when i >= nesting_level ->
-      let v = match nth env (i - nesting_level) with
-        | EnvValue v -> v
-        | EnvThunk l -> Lazy.force l in
-      get_printable_expression nesting_level v 
-  | Index i as e -> e
-  | Proj1 e -> Proj1 (perform_substitutions nesting_level env e)
-  | Proj2 e -> Proj2 (perform_substitutions nesting_level env e)
-  | x -> x
-(* convert a value into an expression which can be printed *)
-and get_printable_expression nesting_level = function
-  | VPair (v1, v2) ->
-      Pair (get_printable_expression nesting_level v1, get_printable_expression
-      nesting_level v2)
-  | VLambda (b, e, env) ->
-      Lambda (b, perform_substitutions (nesting_level + 1) env e)
-  | VPi (b, v, e, env) ->
-      Pi (b, get_printable_expression nesting_level v, perform_substitutions
-      (nesting_level + 1) env e)
-  | VSigma (b, v, e, env) ->
-      Sigma (b, get_printable_expression nesting_level v, perform_substitutions
-      (nesting_level + 1) env e)
-  | VFunction (l, env) -> 
-      Function (map (fun (p, e) -> (p, perform_substitutions
-        (nesting_level + count_pattern_binders p) env e)) l) 
-  | VUniverse -> Universe
-  | VUnitType -> UnitType
-  | VUnit -> Unit
-  | VConstruct (s, l) ->
-      fold_right (fun v e -> Application (e, get_printable_expression
-      nesting_level v))
-        l (Constructor s)
-
-let get_printable_expression = get_printable_expression 0
 
 let rec use_file filename =
   let file = open_in filename in
@@ -170,15 +65,12 @@ and handle_input l = lazy (
   let handle = function
     | Exp e ->
         let exp = desugar_expression !declared e in
-        let evaluated = get_printable_expression (eval !env exp) in
-        print_endline (PrintConcrete.printTree PrintConcrete.prtReplStructure
-          (ReplExpression ((resugar_expression !declared evaluated)
-        , SEMISEMI ";;")))
+        let evaluated = eval !env exp in
+        print_value evaluated
     | Decl d ->
         let new_declared = add_all_declaration_binders !declared d in
         let decl = desugar_declarations !declared d in
         let new_env = add_declarations !env decl in
-        (* TODO: What should be printed here?? *)
         print_endline (PrintConcrete.printTree PrintConcrete.prtReplStructure
           (ReplDeclarations (LLDCons
             (resugar_declarations !declared decl, LLDEmpty), SEMISEMI ";;")));
