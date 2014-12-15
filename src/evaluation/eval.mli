93bb6f51a5fea109561c0e9939ee1c2b21a1a031
Dylan McDermott <dm606@cam.ac.uk>
2014-12-15 14:44:46 +0000

    Added more unit tests

--- src/evaluation/eval.mli
+++ src/evaluation/eval.mli
@@ -2,6 +2,7 @@ open Abstract
 open Value
 
 exception Cannot_evaluate of string
+exception Pattern_match
 
 val add_declarations : value Environment.t -> declaration list
   -> value Environment.t
--- tests/test_eval.ml
+++ tests/test_eval.ml
@@ -23,12 +23,42 @@ let test_tuples = "tuples" >::: (List.map test_eval [
 ]
 
 let test_lambdas = "lambdas" >::: (List.map test_eval [
-
+  ("simple_lambda", Environment.empty, Lambda (Underscore, Proj1 Universe),
+  VLambda (Underscore, Proj1 Universe, Environment.empty));
+  ("simple_application", Environment.empty, Application (Lambda (Name "x", Index
+  0), Unit), VUnit);
+  ("higher_order1", Environment.empty, Application (Lambda (Underscore, Lambda
+  (Name "x", Unit)), Unit), VLambda (Name "x", Unit, Environment.empty));
+  ("higher_order2", Environment.empty, Application (Lambda (Name "x", Index 0),
+  Lambda (Name "y", Unit)), VLambda (Name "y", Unit, Environment.empty))
 ])
 
 let test_patterns = "patterns" >::: (List.map test_eval [
-
-])
+  ("catch_all1", Environment.empty, Application (Function [PatternUnderscore,
+  Unit], Unit), VUnit);
+  ("catch_all2", Environment.empty, Application (Function [PatternBinder "x",
+  Index 0], Unit), VUnit);
+  ("bool1", Environment.empty, Application (Function [PatternApplication
+  ("true", []), Unit; PatternApplication ("false", []), UnitType], Constructor
+  "true"), VUnit);
+  ("bool2", Environment.empty, Application (Function [PatternApplication
+  ("true", []), Unit; PatternApplication ("false", []), UnitType], Constructor
+  "false"), VUnitType);
+  (* (function a _ (_, b x) y _ -> (x, y)) (a U (U, b ()) Unit U) evaluates to
+   * (), Unit *)
+  ("binders", Environment.empty, Application (Function [PatternApplication ("a",
+  [PatternUnderscore; PatternPair (PatternUnderscore, PatternApplication ("b",
+  [PatternBinder "x"])); PatternBinder "y"; PatternUnderscore]), Pair (Index 1,
+  Index 0)], Application (Application (Application (Application (Constructor
+  "a", Universe), Pair (Universe, Application (Constructor "b", Unit))),
+  UnitType), Universe)), VPair (VUnit, VUnitType))
+]) @ [
+  ("no_match" >:: fun _ -> (
+    assert_raises
+      Pattern_match
+      (fun () -> eval Environment.empty (Application (Function [
+          PatternApplication ("a", []), Unit
+        ; PatternApplication ("b", []), Unit], Constructor "c")))))]
 
 let test_eval_other = "eval_other" >::: (List.map test_eval [
 
