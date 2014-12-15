f00a55fcfc57947c26230fedbc86d2efd01eea34
Dylan McDermott <dm606@cam.ac.uk>
2014-12-15 22:03:24 +0000

    Removed unnecessary laziness

--- src/evaluation/environment.ml
+++ src/evaluation/environment.ml
@@ -1,6 +1,6 @@
 open Abstract
 
-type 'a elem = Val of 'a | Decl of declaration list | Thunk of 'a Lazy.t
+type 'a elem = Val of 'a | Decl of declaration list
 
 type 'a t = 'a elem list
 
@@ -15,15 +15,15 @@ let add_declarations env declarations = Decl (List.filter
 
 let get_declarations rest_decls xs = Decl xs
 
-let rec add_declarations_as_thunks eval env =
+let rec add_declarations_as_values eval env =
   let rec add_decls rest rest_decls = function
-    | [] -> List.fold_right (fun t env -> (Thunk t)::env) rest env
+    | [] -> List.fold_right (fun v env -> (Val v)::env) rest env
     | (Let (_, _, e) as d)::xs ->
         let new_env = (Decl (xs @ (List.rev rest_decls)))::env in
-        add_decls ((lazy (eval new_env e))::rest) rest_decls xs
+        add_decls ((eval new_env e)::rest) rest_decls xs
     | (LetRec (_, _, e) as d)::xs as l ->
         let new_env = (Decl (xs @ (List.rev (d::rest_decls))))::env in
-        add_decls ((lazy (eval new_env e))::rest) (d::rest_decls) xs 
+        add_decls ((eval new_env e)::rest) (d::rest_decls) xs 
     | _::xs -> add_decls rest rest_decls xs in
   add_decls [] []
 
@@ -32,18 +32,15 @@ let rec get env eval i = match env, i with
   | [], i -> raise (Failure "get")
   | (Val v)::_, 0 -> v
   | (Val _)::tl, i -> get tl eval (i - 1)
-  | (Thunk t)::_, 0 -> Lazy.force t
-  | (Thunk _)::tl, i -> get tl eval (i - 1)
   | (Decl d)::tl, i -> 
       let l = List.length d in
       if l > i
       then
-        (match List.nth (add_declarations_as_thunks eval tl d) i with
-         | Thunk t -> Lazy.force t
+        (match List.nth (add_declarations_as_values eval tl d) i with
+         | Val v -> v
          | _ -> assert false)
       else get tl eval (i - l)
 
 let map value decl = List.map (function
   | Val v -> value v
-  | Thunk t -> value (Lazy.force t)
   | Decl d -> decl d)
