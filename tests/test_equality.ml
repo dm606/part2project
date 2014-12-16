787cd27f15eb0808c125b8d02786a72fc9a6e78d
Dylan McDermott <dm606@cam.ac.uk>
2014-12-16 13:56:46 +0000

    Forgot to add file

---
+++ tests/test_equality.ml
@@ -0,0 +1,33 @@
+open OUnit2
+open QuickCheck
+
+open Abstract
+open Equality
+open Testing
+
+let test_readback (name, input, expected) =
+  name >:: fun _ -> (assert_equal (readback 0 input) expected)
+
+let test_lambdas = "lambdas" >::: (List.map test_readback [
+
+])
+
+let test_functions = "functions" >::: (List.map test_readback [
+
+])
+
+let test_readback_other = "readback_other" >::: (List.map test_readback [
+
+])
+
+let test_let_equality = quickCheck_test "let_equality"
+  (testable_fun arbitrary_expression show_expression testable_bool)
+  (fun e -> 
+    let e2 = LocalDeclaration ([Let ("x", Universe, e)], Index 0) in
+    try are_equal Environment.empty e e2 with Eval.Cannot_evaluate _ -> true)
+
+let test_equality = "equality" >::: [test_lambdas
+                                   ; test_functions
+                                   ; test_readback_other
+                                   ; test_let_equality
+                                   (*; test_equality_eval*)]
